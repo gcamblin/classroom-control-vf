9cf0399379ac9f2a917abceace6eb95fe46daf0d
Gary Camblin <gcamblin@gmail.com>
2016-02-23 14:33:06 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,6 +43,8 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd"}
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+  creates /etc/motd,
+  }
 }
 }
