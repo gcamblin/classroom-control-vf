fcf4e50c0ed77e975ab3837abdbc8b83a6e1f5a9
Gary Camblin <gcamblin@gmail.com>
2016-02-23 13:04:18 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,11 +43,6 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-  file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Hey, Gary is here!\n",
+exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd"}
 }
 }
