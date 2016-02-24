caa023f1be6986ea77280da60bde4475f4ec92cc
Gary Camblin <gcamblin@gmail.com>
2016-02-24 09:35:59 +0000

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,12 +1,6 @@
 class users {
 user { 'fundamentals':
  			   ensure           => 'present',
-       gid              => '501',
-       home             => '/home/fundamentals',
-       password         => '!!',
-       password_max_age => '99999',
-       password_min_age => '0',
        shell            => '/bin/bash',
-       uid              => '501',
      }
 }
